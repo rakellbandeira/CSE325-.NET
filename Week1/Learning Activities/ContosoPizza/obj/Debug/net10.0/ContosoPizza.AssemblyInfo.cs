40dfe3d2cac0d8dcb73e2ef7226d6d2bda897b2f
rakellbandeira <rakelldesigner@gmail.com>
2026-07-20 23:28:03 -0400

    Use pages, routing, and layouts to improve Blazor navigation

--- Week1/Learning Activities/ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ Week1/Learning Activities/ContosoPizza/obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29643f93a8f79512ffc2a8ca0db5ca2e077b0ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20cd13b0752cb60fbc52eb5bde1dab403f99cc08")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
